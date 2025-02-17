9cd41e29c594d273d20a398c5fd8d6a2749fcf7a
hfz <redptr13@gmail.com>
2025-02-17 18:29:29 +0700

    update dashboard

---
+++ laravels/example-app/resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="PembayaranKulKul\laravels\example-app\resources\css\app.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <h1 class="color text-1xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
